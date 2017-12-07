97973e1e40fca34d43a5ee0a0d5e6a34748c0277
Brian Cilenti <briancilenti@gmail.com>
2017-12-07 12:33:50 -0800

    stopping for lunch

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const burgerIngredient = ( props ) => {
+
+};
+
+export default burgerINgredient;
+
+// we use the curly braces in this stateless functional component because
+// we want to run some logic before rendering JSX.
