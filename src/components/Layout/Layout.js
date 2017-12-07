59d2652c4fe2973c5ab9afd16fd8b0174cbe6942
Brian Cilenti <briancilenti@gmail.com>
2017-12-07 11:58:16 -0800

    Created burger and burger ingredient files/folders. Added custom css to BurgerIngredinet folder.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aus from '../../hoc/Aus';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Aus>
         <div>TODO: Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aus>
@@ -13,6 +14,5 @@ const layout = ( props ) => (
 export default layout;
 // in the main div above, we want to display the components that we
 // we wrap with the layout container.
-
 // we're not allowed to have adjacent elements.  Instead of wrapping
 // with another div, we will create a higher-order-component to wrap.
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Aus from '../../hoc/Aus';
+
+
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aus>
+          <div> Graphical Representaion of build burger </div>
+          <div> Building Controles </div>
+      </Aus>
+    );
+  }
+}
+
+export default BurgerBuilder;
+
+//
