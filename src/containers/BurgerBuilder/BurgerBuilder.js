96cf8c697697cedb33ddb8c0e85e1a7b909c1547
Brian Cilenti <briancilenti@gmail.com>
2017-12-08 13:01:42 -0800

    Completed upto and including lecture 102. Hooked up less and more buttons to add ingredients. set less button to disable if there are no ingredients to remove.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Aus from '../../hoc/Aus';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+// typically name constants you want to use as global constants in ALL CAPS
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
 
 class BurgerBuilder extends Component {
 
@@ -10,16 +19,62 @@ class BurgerBuilder extends Component {
             salad: 0,
             cheese: 0,
             meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        // state should be updated in an immutable way, like so:
+        const updatedIngredients = {
+            ...this.state.ingredients
         }
+        updatedIngredients[type] = updatedCount;
+
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients })
     }
+
+    removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if (oldCount <= 0) {
+            return
+        }
+        const updatedCount = oldCount - 1;
+        // state should be updated in an immutable way, like so:
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        updatedIngredients[type] = updatedCount;
+
+        const priceDeduction = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeduction;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients })
+
+    }
+
     render() {
-    return (
-      <Aus>
-          <Burger ingredients={this.state.ingredients} />
-          <div> Building Controles </div>
-      </Aus>
-    );
-    }
+        // need some logic to disable the "less" button if there is nothing to remove
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = (disabledInfo[key] <= 0)
+        }
+        return (
+          <Aus>
+              <Burger ingredients={this.state.ingredients} />
+              <BuildControls
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}/>
+          </Aus>
+        );
+        }
 }
 
 export default BurgerBuilder;
