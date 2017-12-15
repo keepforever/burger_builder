932ad6da28061ec5ed45e20b72f8e6f60d735257
Brian Cilenti <briancilenti@gmail.com>
2017-12-14 18:02:06 -0800

    completed thru lecture 120.  Now moving on to application refactoring, optimization, and best practices implementing lifecycle methods

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,17 +6,23 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
-
+        showSideDrawer: false
     }
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false })
+    }
+// Due to async nature of setState method, use prevState so you are sure
+// that you are setting state to what the previous state actually was.
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return { showSideDrawer: !prevState.showSideDrawer}
+        });
     }
 
     render () {
         return (
             <Aus>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
