7cf4dd659978873fd592820c2f90a267a9064990
Brian Cilenti <briancilenti@gmail.com>
2018-01-08 23:57:31 -0800

    finished advanced redux module

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -6,34 +6,52 @@ const initialState = {
     loading: false,
     purchased: false
 };
+
+const purchaseInit = (state, action) => {
+    return updateObject(state, {purchased: false})
+}
+const purchaseBurgerStart = (action, state) => {
+    return updateObject(state, {loading: true})
+}
+
+const purchaseBurgerSuccess = (action, state) => {
+    const newOrder = updateObject(action.orderData, {id: action.orderId})
+    return updateObject(state, {
+        loading: false,
+        orders: state.orders.concat(newOrder), // using concat for immutability
+        purchased: true
+    });
+}
+
+const purchaseBurgerFail = (action, state) => {
+    return updateObject(state, {loading: false });
+}
+
+const fetchOrdersStart = (state, action) => {
+    return updateObject(state, {loading: true })
+}
+
+const fetchOrdersSuccess = (state, action) => {
+    return updateObject(state, {
+        orders: action.orders,
+        loading: false
+    })
+}
+const fetchOrdersFail = (state, action) => {
+    return updateObject(state, {loading: false })
+}
+
 // note: we always have a "type" property on any actions passed to reducer
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.PURCHASE_INIT:
-            return updateObject(state, {purchased: false})
-        case actionTypes.PURCHASE_BURGER_START:
-            return updateObject(state, {loading: true})
-        case actionTypes.PURCHASE_BURGER_SUCCESS:
-// need to merge the info passed in with the payload (id and orderData).
-            const newOrder = updateObject(action.orderData, {id: action.orderId})
-            return updateObject(state, {
-                loading: false,
-                orders: state.orders.concat(newOrder), // using concat for immutability
-                purchased: true
-            });
-        case actionTypes.PURCHASE_BURGER_FAIL:
-            return updateObject(state, {loading: false });
-        case actionTypes.FETCH_ORDERS_START:
-            return updateObject(state, {loading: true })
-        case actionTypes.FETCH_ORDERS_SUCCESS:
-            return updateObject(state, {
-                orders: action.orders,
-                loading: false
-            })
-        case actionTypes.FETCH_ORDERS_FAIL:
-            return updateObject(state, {loading: false })
-        default:
-            return state;
+        case actionTypes.PURCHASE_INIT: return purchaseInit(state, action)
+        case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action)
+        case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action)
+        case actionTypes.PURCHASE_BURGER_FAIL: return purchaseBurgerFail(state, action)
+        case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action)
+        case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action)
+        case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action)
+        default: return state;
     }
 }
 
